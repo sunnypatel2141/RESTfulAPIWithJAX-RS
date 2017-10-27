dd5a981738752f9613976ce5b4f96e77c7da3d0c
sunnypatel2141 <sanipatel2141@hotmail.com>
2017-10-27 19:13:02 -0400

    Built Service Stubs

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 Following Java Brain's tutorial to build RESTful APIs with JAX-RS https://www.youtube.com/watch?v=xkKcdK1u95s&list=PLqq-6Pq4lTTZh5U8RbdXq0WaYvZBz2rbn&index=1
 
 Server: Tomcat v7.0
+REST API Client: Postman
---
+++ src/main/java/org/jaxrs/messenger/database/DatabaseClass.java
@@ -0,0 +1,22 @@
+package org.jaxrs.messenger.database;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.jaxrs.messenger.model.*;
+
+public class DatabaseClass
+{
+	//not thread safe
+	private static Map<Long, Message> messages = new HashMap<>();
+	private static Map<Long, Profile> profiles = new HashMap<>();
+	
+	public static Map<Long, Message> getMessages()
+	{
+		return messages;
+	}
+	public static Map<Long, Profile> getProfiles()
+	{
+		return profiles;
+	}
+}
