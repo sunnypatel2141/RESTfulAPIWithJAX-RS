55ef40c076f179465472940a2fc006e47f8c8803
sunnypatel2141 <sanipatel2141@hotmail.com>
2017-10-28 12:15:31 -0400

    Added ability to POST a message

--- src/main/java/org/jaxrs/messenger/resources/MessageResource.java
+++ src/main/java/org/jaxrs/messenger/resources/MessageResource.java
@@ -2,7 +2,9 @@ package org.jaxrs.messenger.resources;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -21,6 +23,14 @@ public class MessageResource
 	public List<Message> getMessages()
 	{
 		return messageService.getAllMessages();
+	}
+	
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Message addMessage(Message message)
+	{
+		return messageService.addMessage(message);
 	}
 	
 	@GET
